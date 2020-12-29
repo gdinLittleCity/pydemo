86e2988096a8fc73438996fbceb3af7296dbeadd
gdinLittleCity <gdinxiao@sina.com>
2020-12-30 01:30:19 +0800

    update

--- baidu_down/baidu_v_resource_down.py
+++ baidu_down/baidu_v_resource_down.py
@@ -7,6 +7,7 @@ import urllib
 import os
 import re
 import getopt
+import time, datetime
 
 def m3u8_down(url, cookie):
     headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36",
@@ -44,6 +45,7 @@ def v_down(cookie):
     line_num = 0
     count = 0
     call_num = 0
+    now = int(time.time())
     for line in soure_list:
         count = count +1
         if line.find("EXT-X-DISCONTINUITY") > 0:
@@ -84,32 +86,33 @@ def v_down(cookie):
                     os.mkdir('input')
                 input_path = os.path.abspath('input')
                 # 写入list文件
-                with open(os.path.join(input_path, 'list.txt'), 'a', encoding="utf-8") as code:
+                with open(os.path.join(input_path, str(now)+"_list.txt"), 'a', encoding="utf-8") as code:
                     code.write("file \'"+file_name+"\' \n")
                 with open(os.path.join(input_path, file_name), "wb") as code:
                     code.write(res.content)
             else:
                 print(res.content)
 
-    ffmpeg_call(out_file_name)
+    ffmpeg_call(out_file_name, now)
 
 
 def write_file_list(file_name):
     print(os.path.abspath(file_name))
 
 
-def ffmpeg_call(out_file_name):
-    list_path = os.path.abspath("list.txt")
+def ffmpeg_call(out_file_name, now):
     input_path = os.path.abspath('input')
-    cmd = "ffmpeg -f concat -safe 0 -i "+os.path.join(input_path, "list.txt")+" -c copy " + os.path.join(input_path, out_file_name)
+    os.chdir(input_path)
+    # cmd = "ffmpeg -f concat -safe 0 -i "+os.path.join(input_path, "list.txt")+" -c copy " + os.path.join(input_path, out_file_name)
+    cmd = "ffmpeg -f concat -safe 0 -i "+str(now)+"_list.txt -c copy " + out_file_name
     print("合并 cmd :[{hcmd}]".format(hcmd=cmd))
     os.system(cmd)
     del_cmd = "del "+ os.path.join(input_path, "*_*.mp4")
-    del_list_cmd = "del " + os.path.join(input_path, "list.txt")
+    # del_list_cmd = "del " + os.path.join(input_path, "list.txt")
     print("delete cmd :[{dele}]".format(dele=del_cmd))
-    print("delete list cmd :[{del_list}]".format(del_list=del_list_cmd))
+    # print("delete list cmd :[{del_list}]".format(del_list=del_list_cmd))
     # 删除视频分片, 视频列表文件
-    # os.system(del_cmd)
+    os.system(del_cmd)
     # os.system(del_list_cmd)
 
 def remove(string):
@@ -133,8 +136,8 @@ def main(argv):
     m3u8_down(url, cookie)
     print("error args: {0}".format(args))
 
-# if __name__ == '__main__':
-#     main(os.sys.argv[1:])
+if __name__ == '__main__':
+    main(os.sys.argv[1:])
 # ffmpeg_call("out.mp4")
 # url = "https://pan.baidu.com/api/streaming?path=%2F02-%E5%9F%BA%E7%A1%80%E7%8F%AD-%E9%83%91%E6%99%93%E5%8D%9A%EF%BC%88%E6%9B%B4%E6%96%B0%E8%87%B3104%E8%AE%B2%EF%BC%89%2F%E8%A7%86%E9%A2%91%2F%E7%AC%AC36%E8%AE%B2%20%C2%A0%C2%A0%20%E5%A4%9A%E9%A1%B9%E8%B5%84%E4%BA%A7%E7%9A%84%E7%BB%84%E5%90%88%E3%80%81%E8%B5%84%E6%9C%AC%E5%B8%82%E5%9C%BA%E7%BA%BF.mp4&app_id=250528&clienttype=0&type=M3U8_FLV_264_480&vip=0&adToken=UlWRIsUUtFdAOvXZ8HktPMfn9joa2hS1VzSqsbo6GdiO%2FyIkgi5EnXo84vyxEn2Ur%2B8O49KiY8GaEKHwzyA7Wz0G4jFjjYX9fg7ywkLyXmZU4LxGWA8%2F%2BvbT8iNB1sbP45lTSNaxAmGhFLhdk9wNLZzZAvTs%2BxOzcrOyy70RuPE%3D"
 # cookie = "PSTM=1565077563; BIDUPSID=0891F37C2E76F2C004C27154D48C7BD8; pan_login_way=1; BAIDUID=000A294E42D801E8F1D8C7EE7642989A:SL=0:NR=10:FG=1; PANWEB=1; MCITY=-%3A; BAIDUID_BFESS=000A294E42D801E8F1D8C7EE7642989A:SL=0:NR=10:FG=1; BDORZ=B490B5EBF6F3CD402E515D22BCDA1598; __yjs_duid=1_063f543b3f8f5fb2223ffa124f056c971609117862039; delPer=0; PSINO=6; H_PS_PSSID=1449_33225_33306_32974_33286_33351_33313_33312_33311_33310_33309_33308_33307_33389_33372_33370; csrfToken=FIlno1UeBDZnRlED7atDgPnQ; Hm_lvt_7a3960b6f067eb0085b7f96ff5e660b0=1608636529,1608637685,1609203364,1609207287; BDUSS=3lIUEg2YXZtYjdWRW9QMnJZU21BeGR6em5Od3laZzhpM0w5N1g2WjdRSlNHeEpnSVFBQUFBJCQAAAAAAQAAAAEAAACO1b4vva3Ez9Chs8cwNTE1AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAFKO6l9SjupfZ; BDUSS_BFESS=3lIUEg2YXZtYjdWRW9QMnJZU21BeGR6em5Od3laZzhpM0w5N1g2WjdRSlNHeEpnSVFBQUFBJCQAAAAAAQAAAAEAAACO1b4vva3Ez9Chs8cwNTE1AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAFKO6l9SjupfZ; SCRC=cb2660f8b2eb14705f0bdf421179a346; STOKEN=6ab305af548b93681ac02324354ab9da932df08562b7e8bce2570abcecf05d34; BDCLND=Y8gUtVPquPHVWrNCBxUFpEdxTSftRB85vHvW68nkIos%3D; Hm_lpvt_7a3960b6f067eb0085b7f96ff5e660b0=1609208260; PANPSC=11043555505473275570%3AKkwrx6t0uHCtza0ZY%2Fm4hBUdqzE5qOrzN7dOlg4IbW2z9Le326YitQPZN16gvceAVaSXcDBdXtjPzW21GgvS0EpDgNqxv39mVgbdZf2zwPuyyezoC8D9eCHshfz3IZqPyIUHUDWcQM3ibxER0vwJhdHhJ5aZPptoX0r3JWTW68DiLaLUl0KD5CiPeEEq9nKY%2BBPsSDpTJNs%3D"
